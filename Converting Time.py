b1c6c3c08587ce217022dfa96e4986c6a8244cc9
Alec-May <105366614+Alec-May@users.noreply.github.com>
2022-06-01 08:16:48 -0400

    Add files via upload

---
+++ Converting Time.py
@@ -0,0 +1,11 @@
+def time_conversion():
+    days = int(input("Please enter days: ")) * 3600 * 24
+    hours = int(input("Please enter hours: ")) * 3600
+    minutes = int(input("Please enter minutes: ")) * 60
+    seconds = int(input("Please enter seconds: "))
+
+    time = days + hours + minutes + seconds 
+
+    print("The Total seconds:", time)
+
+time_conversion()
